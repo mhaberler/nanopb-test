eb349def7d27199cd11de70f11c08ad5cf735939
Michael Haberler <git@mah.priv.at>
2021-10-15 14:35:58 +0200

    add nanopb includes

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+#include <pb_encode.h>
+#include <pb_decode.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
