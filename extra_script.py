da43529b466b500e98a1b9771f41731594724a79
Michael Haberler <git@mah.priv.at>
2021-10-15 14:26:05 +0200

    initial checkin

---
+++ extra_script.py
@@ -0,0 +1,16 @@
+Import("env")
+
+# Single action/command per 1 target
+env.AddCustomTarget("sysenv", None, 'python -c "import os; print(os.environ)"')
+
+# Multiple actions
+env.AddCustomTarget(
+    name="pioenv",
+    dependencies=None,
+    actions=[
+        "pio --version",
+        "python --version"
+    ],
+    title="Core Env",
+    description="Show PlatformIO Core and Python versions"
+)
